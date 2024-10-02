29fa0447cee415b434f0913c44e26a45fd3d6dd3
Kwang1402 <hongquang2k4@gmail.com>
2024-10-02 13:57:52 +0700

    Practical 13: Use thread and handler, simulate a network request and show on a toast

--- USTHWeather/app/src/main/java/vn/edu/usth/weather/WeatherActivity.java
+++ USTHWeather/app/src/main/java/vn/edu/usth/weather/WeatherActivity.java
@@ -3,6 +3,9 @@ package vn.edu.usth.weather;
 import android.content.Intent;
 import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.Message;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -21,6 +24,14 @@ import vn.edu.usth.weather.adapter.WeatherPagerAdapter;
 
 public class WeatherActivity extends AppCompatActivity {
     static final String TAG = "Weather";
+    final Handler handler = new Handler(Looper.getMainLooper()) {
+        @Override
+        public void handleMessage(Message msg) {
+        // This method is executed in main thread
+            String content = msg.getData().getString("server_response");
+            Toast.makeText(getApplicationContext(), content, Toast.LENGTH_SHORT).show();
+        }
+    };
 
     private MediaPlayer mediaPlayer;
     @Override
@@ -44,8 +55,27 @@ public class WeatherActivity extends AppCompatActivity {
 //            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
 //            return insets;
 //        });
-
-
+        Thread t = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                // this method is run in a worker thread
+                try {
+                    // wait for 5 seconds to simulate a long network access
+                    Thread.sleep(5000);
+                }
+                catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                // Assume that we got our data from server
+                Bundle bundle = new Bundle();
+                bundle.putString("server_response", "some sample json here");
+                // notify main thread
+                Message msg = new Message();
+                msg.setData(bundle);
+                handler.sendMessage(msg);
+            }
+        });
+        t.start();
     }
 
     @Override
